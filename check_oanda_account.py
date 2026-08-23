9a5306f61ef4a067cdbefba728da3924a8bb7807
John Li <liqin@hotmail.sg>
2026-08-23 17:59:29 +0800

    dudal_profiles

--- check_oanda_account.py
+++ check_oanda_account.py
@@ -1,7 +1,8 @@
 import oandapyV20
 from oandapyV20.endpoints.accounts import AccountSummary
 from utils.oanda_execution import api
-from config_oanda import OANDA_ACCOUNT_ID
+from config_oanda import OANDA_ACCOUNT_ID_4 as OANDA_ACCOUNT_ID
+OANDA_ACCOUNT_ID = "001-003-21515688-001"
 print(OANDA_ACCOUNT_ID)
 try:
     r = AccountSummary(OANDA_ACCOUNT_ID)
